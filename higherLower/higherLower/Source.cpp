33d3e26f1edb18bb3545d205c2d869e99bf94129
jthai2 <jimmythai35@gmail.com>
2018-04-20 13:15:29 -0500

    higherLower

---
+++ higherLower/higherLower/Source.cpp
@@ -0,0 +1,41 @@
+//Higher Lower
+//4/20/18
+//Jimmy Thai
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>    /* srand, rand*/
+#include <time.h>      /* time */
+#include <random>
+
+int main()
+{
+	int count = 0;
+	int numChoice;
+	int secret;
+	/* initialize random seed: */
+	srand(time(NULL));
+	/* generate secret number between 1 and 10: */
+	secret = rand() % 30 + 1;
+
+	do{
+		std::cout << "Please guess the number between 1-30" << std::endl;
+		std::cin >> numChoice;
+
+		if (numChoice > secret)
+		{
+			std::cout << "The number you have chosen is too high!, guess again loser" << std::endl;
+			count = count + 1;
+		}
+		else if (numChoice < secret)
+		{
+			std::cout << "The number you have chosen is too low!, guess again loser" << std::endl;
+		}
+
+	} while (numChoice != secret);
+
+	std::cout << "Congratulations! You have chosen the number." << std::endl;
+
+	system("pause");
+	return 0;
+}
